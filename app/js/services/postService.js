b09f2bb815139f95186f37834aa489defed8b594
Okiana <oki4102@abv.bg>
2015-05-25 03:45:26 +0300

    Refactor postService.js

--- app/js/services/postService.js
+++ app/js/services/postService.js
@@ -2,40 +2,6 @@ SocialNetwork.factory('postService', function ($http, baseServiceUrl, authServic
 
     var serviceUrl = baseServiceUrl + '/Posts/';
     return {
-            makePost: function (makePostData, success, error) {
-                var request = {
-                    method: 'POST',
-                    url: serviceUrl,
-                    headers: authentication.getAuthHeaders(),
-                    data: makePostData
-                };
-                $http(request).success(success).error(error);
-            },
-            deletePost: function (id, success, error) {
-            var request = {
-                method: 'DELETE',
-                url: serviceUrl + id,
-                headers: authentication.getAuthHeaders()
-            };
-            $http(request).success(success).error(error);
-        },
-        editPost: function (id, editPostData, success, error) {
-            var request = {
-                method: 'PUT',
-                url: serviceUrl + id,
-                headers: authentication.getAuthHeaders(),
-                data:editPostData
-            };
-            $http(request).success(success).error(error);
-        }
-
-    }
-
-});
-SocialNetwork.factory('postService', function ($http, baseServiceUrl, authService, authentication) {
-
-    var serviceUrl = baseServiceUrl + '/Posts/';
-    return {
         makePost: function (makePostData, success, error) {
             var request = {
                 method: 'POST',
